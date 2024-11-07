21a817181443e052d507fa2921d321161b349aff
Michelle Lang Baratto <mi.baratto@gmail.com>
2024-11-07 15:56:50 +0100

    dino project

---
+++ app.js
@@ -0,0 +1,192 @@
+import DINOS from "./dino.js";
+
+const dinosData = DINOS.Dinos;
+
+// Create Dino Constructor
+function Dinossauro(species, weight, height, diet, where, when, fact) {
+  this.species = species;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  this.where = where;
+  this.when = when;
+  this.fact = fact;
+  this.imagem = function () {
+    return "images/" + this.species.toLowerCase() + ".png";
+  };
+}
+
+// Create Dino Objects
+const dinos = dinosData.map((dino) => {
+  let newDinoObj = new Dinossauro(
+    dino.species,
+    dino.weight,
+    dino.height,
+    dino.diet,
+    dino.where,
+    dino.when,
+    dino.fact
+  );
+  return newDinoObj;
+});
+
+function Human(userName, userHeight, userWeight, userDiet) {
+  this.userName = userName;
+  this.userHeight = userHeight;
+  this.userWeight = userWeight;
+  this.userDiet = userDiet;
+}
+
+// Use IIFE to get human data from form
+
+const getHumanData = () => {
+  const userName = document.querySelector("#name").value;
+  const userHeight = document.querySelector("#feet").value;
+  const userWeight = document.querySelector("#weight").value;
+  const userDiet = document.querySelector("#diet").value;
+  const userHumam = new Human(userName, userHeight, userWeight, userDiet);
+  console.log(userHumam);
+  return userHumam;
+};
+
+// Create Dino Compare Methods
+
+const compareWeigth = (dino, human) => {
+  const comparison = dino.weight / human.userWeight;
+  return `The ${dino.species} was ${comparison.toFixed(1)} heavier than you.`;
+};
+
+const compareHeight = (dino, human) => {
+  const comparison = dino.height / human.userHeight;
+  return `The ${dino.species} was ${comparison.toFixed(1)} taller than you.`;
+};
+
+const compareDiet = (dino, human) => {
+  if (dino.diet == human.userDiet) {
+    return `The ${dino.species} was ${dino.diet} like you.`;
+  } else {
+    return `The ${dino.species} was ${dino.diet} different than you ${human.userDiet}.`;
+  }
+};
+
+const showDinoFact = (dino) => {
+  return dino.fact;
+};
+
+const showDinoPeriod = (dino) => {
+  return `The ${dino.species} lived in the ${dino.when} period.`;
+};
+
+const showDinoPlace = (dino) => {
+  return `The ${dino.species} lived in ${dino.where}.`;
+};
+
+const randomNumber = () => {
+  const min = 1;
+  const max = 6;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const chooseFact = (dino, human) => {
+  const resultRandomNumber = randomNumber();
+
+  switch (resultRandomNumber) {
+    case 1:
+      return compareWeigth(dino, human);
+      break;
+    case 2:
+      return compareHeight(dino, human);
+      break;
+    case 3:
+      return compareDiet(dino, human);
+      break;
+    case 4:
+      return showDinoFact(dino);
+      break;
+    case 5:
+      return showDinoPeriod(dino);
+      break;
+    case 6:
+      return showDinoPlace(dino);
+      break;
+  }
+};
+
+const resultChooseFact = chooseFact(dinos[0], getHumanData);
+//console.log("resultChooseFact: " + resultChooseFact);
+
+// Generate Tiles for each Dino in Array
+const dinoTileGeneration = (dino, divToAppend) => {
+  const newTile = document.createElement("div");
+  newTile.setAttribute("class", "grid-item");
+
+  const dinoSpecies = document.createElement("h3");
+  dinoSpecies.textContent = `${dino.species}`;
+  newTile.appendChild(dinoSpecies);
+
+  const dinoImage = document.createElement("img");
+  dinoImage.setAttribute("src", `${dino.imagem()}`);
+  newTile.appendChild(dinoImage);
+
+  divToAppend.appendChild(newTile);
+};
+//HUMAM TILES
+function humanTileGeneration(human, divToAppend) {
+  const newTile = document.createElement("div");
+  newTile.setAttribute("class", "grid-item");
+
+  const humanName = document.createElement("h3");
+  humanName.textContent = `${human.userName}`;
+  newTile.appendChild(humanName);
+
+  divToAppend.appendChild(newTile);
+}
+//adding human Obj(position 4) in array with dinos
+dinos.push(getHumanData);
+console.log(dinos);
+const temp = dinos[8];
+dinos[8] = dinos[4];
+dinos[4] = temp;
+console.log(dinos);
+
+// Add tiles to DOM
+const addTiles = (dinos, human) => {
+  const main = document.querySelector("#grid");
+  for (let i = 0; i < dinos.length; i++) {
+    if (i == 4) {
+      humanTileGeneration(human, main);
+    } else {
+      let newDino = dinos[i];
+      dinoTileGeneration(newDino, main);
+    }
+  }
+};
+
+// Remove form from screen
+const hideForm = () => {
+  const form = document.querySelector("#dino-compare");
+  form.style.display = "none";
+};
+
+// On button click, prepare and display infographic
+const formButton = document.querySelector("#btn");
+formButton.addEventListener("click", () => {
+  const human = getHumanData();
+  hideForm();
+  addTiles(dinos, human);
+});
+
+// *****************************************
+// para cada dino:
+
+// numero aleatorio de 0 a 5
+// array[numero] // altura
+
+// if (propriedade === altura) {
+//   fato = metodoDeALtura(dino, humano)
+// } else if (aaga  === peso) {
+//   fato = metodoDePeso(dino, humano)
+// } else if ( === local) {
+//   fato = metodoLocal(dino)
+// }
+// renderizar -> fato // frase na tela
